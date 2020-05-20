c7b947315d46282cc6664803f5583c7349bfb802
Aayush Goyal <aayush.goyal54@gmail.com>
2020-05-20 19:45:32 +0530

    update

--- codes/ee18btech11001/code2.py
+++ codes/ee18btech11001/code2.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-
+import subprocess
+import shlex
 #Open Loop Transfer function
 A = 1e5
 zeros = []
@@ -15,6 +16,11 @@ plt.xlabel("w")
 plt.grid()
 plt.semilogx(w, mag)    # Bode magnitude plot
 plt.show()
+
+#if using termux
+#plt.savefig('./figs/ee18btech11001/ee18btech11001_1.pdf')
+#plt.savefig('./figs/ee18btech11001/ee18btech11001_1.eps')
+#subprocess.run(shlex.split("termux-open ./figs/ee18btech11001/ee18btech11001_1.pdf"))
 
 #Closed Loop Transfer function
 Num = np.poly(zeros)
@@ -37,6 +43,12 @@ plt.plot(w[0] , mag1[0], 'r.' , markersize=12)
 plt.annotate("( %.2f, %.2f) \n  |H(s)| = %.2f" % (w[0] , mag1[0] , 10**(mag1[0]/20)) , (1.4,mag1[0]-10))
 plt.show()
 
+#if using termux
+#plt.savefig('./figs/ee18btech11001/ee18btech11001_2.pdf')
+#plt.savefig('./figs/ee18btech11001/ee18btech11001_2.eps')
+#subprocess.run(shlex.split("termux-open ./figs/ee18btech11001/ee18btech11001_2.pdf"))
+
+
 #Step Response
 t = np.linspace(0,0.03,1000)
 y = 500*(1 - np.exp(-200*t))
@@ -47,3 +59,8 @@ plt.xlabel("t")
 plt.grid()
 plt.plot(t,np.real(y))
 plt.show()
+
+#if using termux
+#plt.savefig('./figs/ee18btech11001/ee18btech11001_3.pdf')
+#plt.savefig('./figs/ee18btech11001/ee18btech11001_3.eps')
+#subprocess.run(shlex.split("termux-open ./figs/ee18btech11001/ee18btech11001_3.pdf"))
